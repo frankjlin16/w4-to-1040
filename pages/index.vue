7b85bef584bc65c651e62695dc260a50e7f443ab
Frankie J L <67761168+frankjlin16@users.noreply.github.com>
2024-03-01 02:33:34 -0800

    feat: update UI

--- pages/index.vue
+++ pages/index.vue
@@ -20,11 +20,22 @@ async function scan() {
   <Head>
     <Title>{{ title }}</Title>
   </Head>
-  <div>
-    <h1>Image to Text</h1>
-    <input type="text" v-model="imageUrl">
-    <button @click="scan">Scan</button>
-    <p v-if="scanning">Scanning...</p>
-    <p v-else>{{ text }}</p>
+  <div class="container p-4">
+    <h1 class="text-xl">Image to Text</h1>
+    <UContainer class="m-4 w-1/2">
+      <UFormGroup label="Image URL" class="mb-4">
+        <UInput
+          placeholder="https://example-image.com/sample.png"
+          v-model="imageUrl"
+        />
+      </UFormGroup>
+      <div class="flex flex-row-reverse">
+        <UButton @click="scan">Scan</UButton>
+      </div>
+    </UContainer>
+    <UCard class="m-4">
+      <p v-if="scanning">Scanning...</p>
+      <p v-else>{{ text }}</p>
+    </UCard>
   </div>
 </template>

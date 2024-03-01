d0d149d8216c3448265438a98031a34cec18e2f9
Frankie J L <67761168+frankjlin16@users.noreply.github.com>
2024-03-01 00:41:04 -0800

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const title = ref('W2 to 1040')
+</script>
+
+<template>
+  <Head>
+    <Title>{{ title }}</Title>
+  </Head>
+  <div>
+    <h1>Homepage</h1>
+  </div>
+</template>
